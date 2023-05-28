963e2989b3e56e57873c5d50321401127b5f3342
Muhammad Usman Mughal <misterusman3@gmail.com>
2023-05-28 13:13:38 +0500

    Login Page added

--- src/Components/Nav.tsx
+++ src/Components/Nav.tsx
@@ -1,25 +1,29 @@
+import { Link } from "react-router-dom";
 import Logo from "../assets/logo.png";
 import DelivIvon from "../assets/NavDeliveryicon.png";
 import PickUpIcon from "../assets/NavPickupICon.png";
 import { BiCurrentLocation } from "react-icons/bi";
+import e from "../assets/bucket-empty (1).png";
 const Nav = () => {
   return (
-    <div className="">
-      <nav className="bg-black fixed top-0 left-0 right-0  z-50 py-3  ">
+    <div className=" ">
+      <nav className="bg-black    py-3  ">
         <div className="w-4/5 m-auto border-3 border-white-900 flex justify-between  ">
           <div className="flex-col  items-center    gap-5   md:flex  lg:flex-row xl:flex   ">
             <div className="logo ">
-              <img
-                src={Logo}
-                alt="Logo"
-                className="hidden  sm:block lg:block xl:block "
-              />
+              <Link to="/">
+                <img
+                  src={Logo}
+                  alt="Logo"
+                  className="hidden  w-24 sm:block lg:block xl:block "
+                />
+              </Link>
               <h1 className="font-bold text-white text-3xl sm:hidden lg:hidden xl:hidden ">
                 KFC
               </h1>
             </div>
-            <div className="font-bold text-xs sm:w-full flex  bg-stone-800 text-white rounded-full">
-              <span className="bg-stone-500 p-1 px-2 flex items-center rounded-md">
+            <div className="font-bold text-xs sm:w-full flex  bg-stone-900 text-white rounded-full">
+              <span className="bg-stone-700 p-1 px-2 flex items-center rounded-md">
                 {" "}
                 <img src={DelivIvon} alt="Delivery" className="px-2" /> DELIVERY
               </span>
@@ -34,11 +38,13 @@ const Nav = () => {
               <BiCurrentLocation className=" text-red-600 text-2xl" /> Select
               Location
             </div>
-            <div className="  border-2 px-4 text-lg  ">0</div>
+            <div className="     ">
+              <img src={e} alt="" className="w-8" />
+            </div>
 
             <div>
               <button className="bg-red-600 rounded-md py-1 px-2  font-semibold hover:bg-red-700 ">
-                Register / Sign in
+                <Link to="/login "> Register / Sign in</Link>
               </button>
             </div>
           </div>
